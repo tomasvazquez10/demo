6708536b8f2a34eda6a7d1a90b3c7f0f3f8d14e7
Tomas Vazquez <tomas.vazquez@ing.austral.edu.ar>
2024-05-09 20:22:27 -0300

    agrego configuracion base sql server

--- src/main/java/com/emergentes/demo/controller/DatoController.java
+++ src/main/java/com/emergentes/demo/controller/DatoController.java
@@ -1,18 +1,36 @@
 package com.emergentes.demo.controller;
 
 import com.emergentes.demo.model.DatoCensado;
+import com.emergentes.demo.repository.DatoCensadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/demo")
-public class Controller {
+public class DatoController {
+
+    private final DatoCensadoRepository repository;
+
+    @Autowired
+    public DatoController(DatoCensadoRepository repository) {
+        this.repository = repository;
+    }
 
     @RequestMapping("/test")
     public ResponseEntity<String> getTest(){
         return new ResponseEntity<>("Demo test", HttpStatus.OK);
+    }
+
+    @RequestMapping("/dato/{id}")
+    public ResponseEntity<DatoCensado> getDatoCensado(@PathVariable Long id){
+        Optional<DatoCensado> optionalDatoCensado = repository.findById(id);
+        return optionalDatoCensado.map(datoCensado -> new ResponseEntity<>(datoCensado, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(new DatoCensado(), HttpStatus.NOT_FOUND));
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/test")
@@ -24,6 +42,8 @@ public class Controller {
     @RequestMapping(method = RequestMethod.POST, value = "/dato")
     public ResponseEntity<DatoCensado> saveData(@RequestBody DatoCensado dato){
         System.out.println("Dato enviado: "+dato.toString());
-        return new ResponseEntity<>(dato, HttpStatus.OK);
+        DatoCensado nuevoDato = repository
+                .save(dato);
+        return new ResponseEntity<>(nuevoDato, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/emergentes/demo/repository/DatoCensadoRepository.java
@@ -0,0 +1,8 @@
+package com.emergentes.demo.repository;
+
+import com.emergentes.demo.model.DatoCensado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DatoCensadoRepository extends JpaRepository<DatoCensado, Long> {
+
+}
